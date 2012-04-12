5e86791d41418463217a08d9dc67becc42e13500
Mikael Jorhult <mikael@jorhult.se>
2012-04-12 21:38:05 +0200

    Initial commit

---
+++ autun.js
@@ -0,0 +1,80 @@
+/**
+ * autun
+ * minimalistic attribute manipulation.
+ * 
+ * @version 0.1
+ * @author Mikael Jorhult
+ * @license http://mikaeljorhult.mit-license.org MIT
+ *
+ * Project repository: https://github.com/mikaeljorhult/autun
+ */
+autun = {
+	addClass: function(name, object) {
+		if(object) { object = this.objectArray(object); };
+		
+		for(var i = 0; i < object.length; i++) {
+			var value = this.attr('class', undefined, object[i]);
+			this.attr('class', value + ' ' + name, object[i]);
+		}
+		
+		return object;
+	},
+	
+	attr: function(attribute, value, object) {
+		if(object) { object = this.objectArray(object); }
+		
+		if(value) {
+			object[0].setAttribute(attribute, value);
+		} else {
+			return object[0].getAttribute(attribute);
+		}
+		
+		return object;
+	},
+	
+	hasClass: function(name, object) {
+		if(object) { object = this.objectArray(object); }
+		
+		return (this.attr('class', undefined, object).split(' ').indexOf(name) > -1 ? true : false);
+	},
+	
+	html: function(object) {
+		if(object) { object = this.objectArray(object); };
+		
+		return text = object[0].innerHTML;
+	},
+	
+	removeClass: function(name, object) {
+		if(object) { object = this.objectArray(object); };
+		
+		for(var i = 0; i < object.length; i++) {
+			var value = this.attr('class', undefined, object[i])
+				.replace(new RegExp('(\\s|^)' + name + '(\\s|$)', 'i'), '');
+			this.attr('class', value, object[i]);
+		}
+		
+		return object;
+	},
+	
+	toggleClass: function(name, object) {
+		if(object) { object = this.objectArray(object); };
+		
+		return (this.hasClass(name, object) ? this.removeClass(name, object) : this.addClass(name, object));
+	},
+	
+	val: function(object) {
+		if(object) { object = this.objectArray(object); };
+		
+		return text = object[0].innerText || object[0].textContent;
+	},
+	
+	objectArray: function(object) {
+		if(object.constructor.toString().indexOf('Array') > -1) {
+			return object;
+		} else if(object.constructor.toString().indexOf('HTMLCollection') > -1) {
+			return Array.prototype.slice.call(object);
+		} else {
+			return new Array(object);
+		}
+	}
+};
