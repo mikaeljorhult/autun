62c798ec1ac00818b0f3e31004114a2db6611cd1
Mikael Jorhult <mikael@jorhult.se>
2012-04-15 15:03:38 +0200

    Let other library handle arrays

--- autun.js
+++ autun.js
@@ -2,7 +2,7 @@
  * autun
  * minimalistic attribute manipulation.
  * 
- * @version 0.1.4
+ * @version 0.1.5
  * @author Mikael Jorhult
  * @license http://mikaeljorhult.mit-license.org MIT
  *
@@ -12,8 +12,6 @@ autun = {
 	class_list: null,
 	
 	addClass: function(name, object) {
-		if(object) { object = this.objectArray(object); };
-		
 		for(var i = 0, length = object.length; i < length; i++) {
 			if(this.class_list) {
 				object[i].classList.add(name);
@@ -26,8 +24,6 @@ autun = {
 	},
 	
 	attr: function(attribute, value, object) {
-		if(object) { object = this.objectArray(object); }
-		
 		if(value) {
 			for(var i = 0, length = object.length; i < length; i++) {
 				object[i].setAttribute(attribute, value);
@@ -40,8 +36,6 @@ autun = {
 	},
 	
 	hasClass: function(name, object) {
-		if(object) { object = this.objectArray(object); }
-		
 		if(this.class_list) {
 			return object[0].classList.contains(name);
 		} else {
@@ -50,14 +44,10 @@ autun = {
 	},
 	
 	html: function(object) {
-		if(object) { object = this.objectArray(object); };
-		
 		return text = object[0].innerHTML;
 	},
 	
 	removeClass: function(name, object) {
-		if(object) { object = this.objectArray(object); };
-		
 		var regexp_class = new RegExp('(\\s|^)' + name + '(\\s|$)', 'i');
 		
 		for(var i = 0, length = object.length; i < length; i++) {
@@ -72,8 +62,6 @@ autun = {
 	},
 	
 	toggleClass: function(name, object) {
-		if(object) { object = this.objectArray(object); };
-		
 		if(this.class_list) {
 			for(var i = 0, length = object.length; i < length; i++) {
 				object[i].classList.toggle(name);
@@ -86,8 +74,6 @@ autun = {
 	},
 	
 	val: function(object) {
-		if(object) { object = this.objectArray(object); };
-		
 		return text = object[0].innerText || object[0].textContent;
 	},
 	
@@ -97,15 +83,5 @@ autun = {
 		
 		// Return object
 		return this;
-	},
-	
-	objectArray: function(object) {
-		if(object.constructor.toString().indexOf('Array') > -1) {
-			return object;
-		} else if(object.constructor.toString().match(/NodeList|HTMLCollection/)) {
-			return Array.prototype.slice.call(object);
-		} else {
-			return new Array(object);
-		}
 	}
 }.init();
